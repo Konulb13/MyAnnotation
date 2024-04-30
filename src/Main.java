bec5b5676d33b4dcd59e225f42e45035adf99b3b
Konulb13 <143033792+Konulb13@users.noreply.github.com>
2024-04-30 21:31:29 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import java.lang.reflect.Method;
+
 public class Main {
     public static void main(String[] args) {
-        @MyAnnotation(a = 42,b =38)
-        public void annMethod (int a,int b){
-            System.out.println(a+b);
+        MyAnnClass an = new MyAnnClass();
+        Class<?> cl = an.getClass();
+        Method [] allMethods = cl.getDeclaredMethods();
+        for (Method m : allMethods){
+            MyAnnotation myAnnotation = m.getAnnotation(MyAnnotation.class);
+            int a = myAnnotation.a();
+            int b = myAnnotation.b();
+            an.annMethod(a,b);
+        }
+
     }
 
 }
-
+ class MyAnnClass{
+    @MyAnnotation(a = 42,b = 38)
+    public void annMethod (int a,int b){
+        System.out.println(a+b);
     }
+ }
