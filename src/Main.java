b53c6b46c49cd41d8d357a2e23bc8656a0d4a194
Konulb13 <143033792+Konulb13@users.noreply.github.com>
2024-05-02 21:42:47 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
         MyAnnClass an = new MyAnnClass();
         Class<?> cl = an.getClass();
-        Method [] allMethods = cl.getDeclaredMethods();
-        for (Method m : allMethods){
-            MyAnnotation myAnnotation = m.getAnnotation(MyAnnotation.class);
-            int a = myAnnotation.a();
-            int b = myAnnotation.b();
-            an.annMethod(a,b);
+        Method[] allMethods = cl.getDeclaredMethods();
+        for (Method m : allMethods) {
+            if (m.isAnnotationPresent(MyAnnotation.class)) {
+                MyAnnotation myAnnotation = m.getAnnotation(MyAnnotation.class);
+                System.out.println(m.invoke(an, myAnnotation.a(), myAnnotation.b()));
+            }
+
         }
-
     }
-
 }
-class MyAnnClass{
-    @MyAnnotation(a = 42,b = 38)
-    public void annMethod (int a,int b){
-        System.out.println( "a = " + a +"\n" +"b = " + b );
+
+class MyAnnClass {
+    @MyAnnotation(a = 42, b = 38)
+    public int annMethod(int a, int b) {
+        System.out.println("a = " + a + "\n" + "b = " + b);
+        return a+b;
     }
- }
+}
